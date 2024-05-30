8f55d290e2e89690d4c41d56900dc81d756e4049
Claus-Christoph Küthe <github@vm01.telton.de>
2024-05-30 17:48:05 +0200

    Added Countdown to end program after 10 seconds.
    I'm about to make changes to RawConsoleInput. RCI itself catches CTRL+C, so
    if something is wrong with the exit mechanism used, the program will run
    indefinitely. So let's shut it down after 10 seconds.

--- src/main/java/com/corvidus/prototyping/Threads.java
+++ src/main/java/com/corvidus/prototyping/Threads.java
@@ -2,22 +2,28 @@ package com.corvidus.prototyping;
 
 import com.corvidus.input.Input;
 import com.corvidus.input.InputObserver;
+import com.corvidus.prototyping.threads.Countdown;
+import com.corvidus.prototyping.threads.CountdownObserver;
 import com.corvidus.prototyping.threads.StringWriter;
-public class Threads implements InputObserver {
+public class Threads implements InputObserver, CountdownObserver {
 	private StringWriter sw01;
 	private StringWriter sw02;
 	private Input input;
+	private Countdown countdown;
 	public Threads() {
 		this.sw01 = new StringWriter("I like dogs.");
 		this.sw02 = new StringWriter("I like cats.");
 		this.input = new Input();
+		this.countdown = new Countdown(10000);
 	}
 	
 	public void run() {
 		this.input.setInputObserver(this);
+		this.countdown.setCountdownObserver(this);
 		this.sw01.start();
 		this.sw02.start();
 		this.input.start();
+		this.countdown.start();
 	}
 	public static void main(String[] args) {
 		Threads threads = new Threads();
@@ -29,5 +35,11 @@ public class Threads implements InputObserver {
 		if(inString.equals("x")) {
 			System.exit(0);
 		}
+	}
+
+	@Override
+	public void onEnd(Countdown countdown) {
+		System.out.println();
+		System.exit(0);
 	}
 }
