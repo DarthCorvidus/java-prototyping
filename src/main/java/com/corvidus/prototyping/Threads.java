23b5ca25521f1ec98e54f99fda0cc5d0c8ae4699
Claus-Christoph Küthe <github@vm01.telton.de>
2024-05-30 14:16:16 +0200

    Added Input thread to listen for user input. Exit on 'x'.

--- src/main/java/com/corvidus/prototyping/Threads.java
+++ src/main/java/com/corvidus/prototyping/Threads.java
@@ -1,11 +1,24 @@
 package com.corvidus.prototyping;
 
+import com.corvidus.input.Input;
+import com.corvidus.input.InputObserver;
 import com.corvidus.prototyping.threads.StringWriter;
-public class Threads {
+public class Threads implements InputObserver {
 	public static void main(String[] args) {
 		StringWriter sw01 = new StringWriter("I like dogs.");
 		StringWriter sw02 = new StringWriter("I like cats.");
+		Input input = new Input();
+		input.setInputObserver(new Threads());
 		sw01.start();
 		sw02.start();
+		input.start();
+		
     }
+
+	@Override
+	public void onInput(Input input, String inString) {
+		if(inString.equals("x")) {
+			System.exit(0);
+		}
+	}
 }
