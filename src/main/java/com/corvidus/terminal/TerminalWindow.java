841bd63963c01a24aa2a85ffb40fd7cac73fcdcd
Claus-Christoph Küthe <github@vm01.telton.de>
2024-05-31 21:15:03 +0200

    Actually adding some content to the terminal: it flips through the alphabet.

--- src/main/java/com/corvidus/terminal/TerminalWindow.java
+++ src/main/java/com/corvidus/terminal/TerminalWindow.java
@@ -2,16 +2,26 @@ package com.corvidus.terminal;
 public class TerminalWindow {
 	private int width;
 	private int height;
-	private String buffer;
+	private String rotate = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private int page = 0;
 	public TerminalWindow(int width, int height) {
 		this.width = width;
 		this.height = height;
-		this.buffer = "A".repeat(this.width*this.height);
+	}
+	
+	private String getBuffer() {
+		String buffer = this.rotate.substring(this.page, this.page+1).repeat(this.width*this.height);
+		if(this.page<this.rotate.length()) {
+			this.page++;
+		} else {
+			this.page = 0;
+		}
+	return buffer;
 	}
 	
 	public void draw() {
 		System.out.print((char)27+"[2J");
 		System.out.print((char)27+"[H");
-		System.out.print(this.buffer);
+		System.out.print(this.getBuffer());
 	}
 }
