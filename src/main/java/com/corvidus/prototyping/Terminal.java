03764ffaacf601d263cf9576dd91f9c9a1ad3e22
Claus-Christoph Küthe <github@vm01.telton.de>
2024-05-31 21:52:44 +0200

    The redraw delay can be changed by pressing + and -.
    Below 100, it gets changed in increments of 10 milliseconds, above in 100.
    
    On my system, a local terminal starts to flicker noticeably when using values
    below 50 ms.

--- src/main/java/com/corvidus/prototyping/Terminal.java
+++ src/main/java/com/corvidus/prototyping/Terminal.java
@@ -7,6 +7,7 @@ import com.corvidus.terminal.TerminalWindow;
 public class Terminal implements InputObserver {
 	private Input input;
 	private TerminalWindow tw;
+	private int delay = 1000;
 	public Terminal() {
 		this.input = new Input();
 		this.tw = new TerminalWindow(80, 24);
@@ -17,8 +18,10 @@ public class Terminal implements InputObserver {
 		this.input.start();
 		while(true) {
 			this.tw.draw();
+			System.out.print((char)27+"[H");
+			System.out.print(this.delay);
 			try {
-				Thread.sleep(1000, 0);
+				Thread.sleep(this.delay, 0);
 			} catch(InterruptedException e) {
 				e.printStackTrace();
 				System.exit(0);
@@ -35,7 +38,24 @@ public class Terminal implements InputObserver {
 	public void onInput(Input input, char c) {
 		if(c == 'x' || c == (char)3) {
 			this.input.interrupt();
+			System.out.print((char)27+"[2J");
+			System.out.print((char)27+"[H");
 			System.exit(0);
+		}
+		if(c == '+' && this.delay > 100) {
+			this.delay -= 100;
+		}
+
+		if(c == '+' && this.delay > 10 && this.delay <= 100) {
+			this.delay -= 10;
+		}
+
+		if(c == '-' && this.delay <= 100) {
+			this.delay += 10;
+		}
+		
+		if(c == '-' && this.delay > 100) {
+			this.delay += 100;
 		}
 	}
 }
